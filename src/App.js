eafb3547c6a9c163fc0157a72690ca65fa07390f
Amrabdo74 <88573123+Amrabdo74@users.noreply.github.com>
2022-08-28 03:07:15 +0200

    first templet by react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Routes ,Route} from 'react-router-dom'
 import './App.css';
+import Navbar from './Components/Navbar';
+import Contact from './Components/Contact';
+import Index from './Components/Index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+        <Route  path='/' element={<Index/> } />
+        <Route path='/contact' element={<Contact/>} />
+        </Routes>
+      </BrowserRouter>
+    )
+  }
 }
-
 export default App;
